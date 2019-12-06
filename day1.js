ffbad6fd2efd5db0ab61ab4a9ca0cb826f4ca6cc
GabrielQSherman <56276327+GabrielQSherman@users.noreply.github.com>
2019-12-05 23:10:29 -0500

    Add files via upload
    triangles and circles being created by funcitons

---
+++ day1.js
@@ -0,0 +1,64 @@
+
+window.onload = function () {
+
+   var canvas = document.getElementById("canvas"),
+        context = canvas.getContext("2d"),
+        width = canvas.width = this.innerWidth, //this refers to window
+        height = canvas.height = this.innerHeight,
+        time = 0;
+
+        context.strokeStyle = 'black';
+
+        // context.beginPath();
+
+        // context.moveTo(width, height);
+        // context.lineTo(100,0);
+
+        // context.stroke();
+
+        context.translate(width/2, height/2);
+
+
+        function createTri(sizeLength) {
+            
+            for (let i = 0; i < 3; i++) {
+                context.beginPath();
+
+                context.moveTo(sizeLength, sizeLength);
+
+                context.rotate((120 * Math.PI)/180);
+
+                context.lineTo(sizeLength, sizeLength);
+
+                context.stroke()
+                
+            }
+        }
+
+        function createCircle(radius) {
+            
+        }
+
+        var size = 10;
+
+        for (let i = 0; i < 100; i++) {
+
+            size += 10;
+            
+            if (i % 2 == 0) {
+
+                createTri(size);
+                
+            } else {
+
+
+            }
+
+            size += 100;
+            
+        }
+
+
+        
+
+}
