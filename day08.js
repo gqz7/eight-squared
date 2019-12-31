8f699e627589c7c27ccc78c6ac17e8700d3c8222
GabrielQSherman <56276327+GabrielQSherman@users.noreply.github.com>
2019-12-30 22:04:30 -0500

    tree growth complete

--- day08.js
+++ day08.js
@@ -2,31 +2,74 @@ window.onload = () => {
 
     const canvas = document.getElementById('canvas'),
     ctx = canvas.getContext('2d'),
-    width = canvas.width = 1700,
-    height = canvas.height = 900;
+    width = canvas.width = innerWidth,
+    height = canvas.height = innerHeight;
 
-    ctx.strokeStyle = "white"
+    let time = 100,
+        angle = Math.PI / 2;
 
-    //setup/define the root of the tree
+        let i = 0;
+
+    ctx.strokeStyle = "white";
+    ctx.translate(width/2, height);
+
+    function createLine(x1,y1,x2,y2) {
+        ctx.beginPath()
+        ctx.moveTo(x1, y1)
+        ctx.lineTo(x2, y2)
+        ctx.stroke()
+    }
+
+    function clear() {
+        ctx.save();
+        ctx.setTransform(1, 0, 0, 1, 0, 0);
+        ctx.clearRect(0, 0, canvas.width, canvas.height);
+        ctx.restore();
+    }
+   
+    function create_tree(angle, length, interations) {
+        
+        createLine(0,0,0,-length);
+
+        ctx.translate(0, -length);
+        length *= .67;
     
+        if(length > 3) { 
+        ctx.save()
+        ctx.rotate(angle)
+        create_tree(angle, length); 
 
+        ctx.restore()
+        ///////////////
+        
+        //////////////
+        ctx.save()
+    
+        ctx.rotate(-angle)
+        create_tree(-angle, length);
+        ctx.restore()
+
+        }
+    }
 
     //animate the tree
     render()
         function render() {
-            time += .2;
+            clear()
+            
+            time++;
+            angle -= .003;
 
-            if (time > 240) {
+            ctx.strokeStyle = 'hsl(' + (time * 1) + ', 100%, 65%)';
 
-                // context.save();
-                // context.setTransform(1, 0, 0, 1, 0, 0);
-                // context.clearRect(0, 0, canvas.width, canvas.height);
-                // context.restore();
-                // time = 0;
-                
-            } else {
-                 setTimeout( window.requestAnimationFrame, 0, (render));
-            }
+            // console.log(time)
+
+            ctx.save()
+            create_tree(angle, 50 + time/2, 3);
+            ctx.restore()
+            
+            setTimeout( window.requestAnimationFrame, 0, (render));
+            
         }
 
 }
