8c43b0296727bcdc250d026fd1e40d24daa778fc
GabrielQSherman <56276327+GabrielQSherman@users.noreply.github.com>
2019-12-30 19:31:26 -0500

    Add files via upload

--- day08.js
+++ day08.js
@@ -5,13 +5,28 @@ window.onload = () => {
     width = canvas.width = 1700,
     height = canvas.height = 900;
 
-    ctx.strokeStyle = "white";
+    ctx.strokeStyle = "white"
 
-    ctx.beginPath();
-    ctx.moveTo(100,100);
-    ctx.lineTo(200,300);
-    ctx.stroke();
-
+    //setup/define the root of the tree
     
+
+
+    //animate the tree
+    render()
+        function render() {
+            time += .2;
+
+            if (time > 240) {
+
+                // context.save();
+                // context.setTransform(1, 0, 0, 1, 0, 0);
+                // context.clearRect(0, 0, canvas.width, canvas.height);
+                // context.restore();
+                // time = 0;
+                
+            } else {
+                 setTimeout( window.requestAnimationFrame, 0, (render));
+            }
+        }
 
 }
