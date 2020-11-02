42c2c869201830a0c5cf3d414df25473a976ce74
GabrielQSherman <gabrielquinn.photography@gmail.com>
2020-11-02 17:05:24 -0500

    create both the 1D and 2D Peice array together and create peices based on position, then add the newly created peices to Player objects

--- GameLogic/Board.java
+++ GameLogic/Board.java
@@ -12,33 +12,66 @@ public class Board {
     private BoardPlace[][] gameSpace2D;
 
     public Board(Player player1, Player player2) {
-
-        gameSpace = createBoardPlaces1D();
-        gameSpace2D = createBoardPlaces2D();
+        BoardPlace[][][] boardData = initializeBoard( player1, player2 );
+        gameSpace = boardData[0][0];
+        gameSpace2D = boardData[1];
 
     }
 
-    private BoardPlace[] createBoardPlaces1D() {
-        BoardPlace[] startingBoard = new BoardPlace[totalPlaces];
+    private BoardPlace[][][] initializeBoard (Player player1, Player player2) {
+        BoardPlace[] startingBoard1D = new BoardPlace[totalPlaces];
+        BoardPlace[][] startingBoard2D = new BoardPlace[totalRows][totalColumns];
 
-        for (int i = 0; i < totalPlaces; i++) {
-            int row = (i/8) + 1;
-            int colNum = (i%8) + 1;
-//            System.out.println("Row: " + row + " Col: " + colNum);
-            startingBoard[i] = new BoardPlace( row, colNum );
-        }
-        return startingBoard;
-    }
-
-    private BoardPlace[][] createBoardPlaces2D() {
-        BoardPlace[][] startingBoard = new BoardPlace[totalRows][totalColumns];
-
+        int count = 0; //count keeps track of the index of the single dimension array
         for (int i = 0; i < totalRows; i++) {
             for (int j = 0; j < totalColumns; j++) {
-                startingBoard[i][j] = new BoardPlace( i+1, j+1 );
+                //create a new space for pieces to reside
+                BoardPlace newSpace = new BoardPlace( i+1, j+1 );
+                //create a new pieces based on the location of the space
+                Piece newPiece = getSpacesPiece(newSpace);
+                //add the piece to the space
+                newSpace.holding = newPiece;
+
+                //add newly created Piece to players pieces array
+                if (newPiece != null){
+                    if (newPiece.isWhite)
+                        player1.addPiece(newPiece);
+                    else
+                        player2.addPiece(newPiece);
+                }
+                //add space to board arrays
+                startingBoard2D[i][j] = newSpace;
+                startingBoard1D[count] = newSpace;
+                count++;
             }
         }
-        return startingBoard;
+
+        return new BoardPlace[][][] { {startingBoard1D}, startingBoard2D};
+    }
+
+    private Piece getSpacesPiece(BoardPlace startPos) {
+        if (startPos.row > 2 && startPos.row < 7 ) return null;
+
+        if (startPos.row == 2 || startPos.row == 7 ) return new Pawn(startPos);
+
+        switch (startPos.columnInt) {
+            case 1:
+            case 8:
+                return new Rook(startPos);
+            case 2:
+            case 7:
+                return new Knight(startPos);
+            case 3:
+            case 6:
+                return new Bishop(startPos);
+            case 4:
+                return new Queen(startPos);
+            case 5:
+                return new King(startPos);
+            default:
+                System.out.println("An Invalid Position Was Given to 'getSpacePieces' in Board");
+                return null;
+        }
     }
 
     @Override
