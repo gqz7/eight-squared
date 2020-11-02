1d8e043e3d02757c10b7c70ce744b2e35b303930
GabrielQSherman <gabrielquinn.photography@gmail.com>
2020-11-02 14:41:29 -0500

    flesh out the abstract class of Piece with position, color and inGame booleans

--- GameLogic/Piece.java
+++ GameLogic/Piece.java
@@ -1,4 +1,15 @@
 package com.company;
 
 public abstract class Piece implements Movable {
+    public boolean isInGame;
+    public boolean isWhite;
+    public BoardPlace position;
+
+    public Piece( BoardPlace startPosition, boolean isWhite) {
+        position = startPosition;
+        isInGame = true;
+        this.isWhite = isWhite;
+    }
+    @Override
+    abstract public String toString();
 }
