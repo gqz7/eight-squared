ea43356fd8c1e2c161ee3a1334387628ca32bd9b
GabrielQSherman <gabrielquinn.photography@gmail.com>
2020-11-02 17:06:24 -0500

    add a method to add more pieces to a player and a toString method override to display all the players pieces

--- GameLogic/Player.java
+++ GameLogic/Player.java
@@ -3,10 +3,28 @@ package com.company;
 public class Player {
     private boolean isWhite;
     private Piece[] pieces;
+    private int totalPieces;
 
     public Player (boolean isWhite) {
         this.isWhite = isWhite;
-        this.pieces = new Piece[16];
+
+        totalPieces = 0;
+        pieces = new Piece[16];
+
+    }
+
+    public void addPiece (Piece newPiece ) {
+        pieces[totalPieces] = newPiece;
+        totalPieces++;
+    }
+
+    @Override
+    public String toString() {
+        String playersPieces = "";
+        for ( Piece gamePiece : pieces ) {
+            playersPieces += (gamePiece + ": " + gamePiece.position.notation + ": isInGame; " + gamePiece.isInGame + "\n");
+        }
+        return playersPieces;
     }
 
 }
