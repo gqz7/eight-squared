8430f19b66ad5e92a2b5f1bdcb3e430546955f0b
GabrielQSherman <gabrielquinn.photography@gmail.com>
2020-11-02 22:01:10 -0500

    create a method to take user input to make a move

--- GameLogic/Player.java
+++ GameLogic/Player.java
@@ -1,5 +1,7 @@
 package com.company;
 
+import java.util.Arrays;
+
 public class Player {
     private boolean isWhite;
     private Piece[] pieces;
@@ -27,4 +29,25 @@ public class Player {
         return playersPieces;
     }
 
+    public Turn makeTurn() {
+
+        Object[] filtered = Arrays.stream(pieces).filter( piece -> piece.isInGame).toArray();
+        Piece[] playablePieces = new Piece[filtered.length];
+
+        for (int i = 0; i < filtered.length; i++) {
+            if (filtered[i] instanceof Piece) {
+                playablePieces[i] = (Piece) filtered[i];
+            }
+        }
+
+        System.out.println("Select A Piece To Move...");
+        for (int i = 0; i < playablePieces.length; i++) {
+            Piece p = playablePieces[i];
+            System.out.println(i+1 + ") " + p.getName() + ", " + p.position.notation);
+        }
+
+        int selectedPiece = CLI.numberIntQuestion("Enter a number", 1, playablePieces.length);
+
+        return new Turn( playablePieces[0], playablePieces[0].position);
+    }
 }
