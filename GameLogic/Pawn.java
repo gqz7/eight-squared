ce47791880a847748305fc456fd9949b66c5a404
GabrielQSherman <gabrielquinn.photography@gmail.com>
2020-11-03 22:28:58 -0500

    complete working algorithm to calculate pawn possible moves

--- GameLogic/Pawn.java
+++ GameLogic/Pawn.java
@@ -1,5 +1,8 @@
 package com.company;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Pawn extends Piece {
 
     private String name;
@@ -15,6 +18,7 @@ public class Pawn extends Piece {
     }
 
 
+
     @Override
     public String toString() {
         return (this.isWhite ? "W" : "B") + "P";
@@ -23,5 +27,45 @@ public class Pawn extends Piece {
     @Override
     public String getName() {
         return name;
+    }
+
+    @Override
+    public BoardPlace[] getPossibleMoves( Board board) {
+
+        Piece movingPiece = this;
+
+        List<BoardPlace> movesList = new ArrayList<>();
+        boolean isWhite = movingPiece.isWhite;
+        BoardPlace piecePos = movingPiece.position;
+
+        int tempRow = piecePos.row;
+        //int tempCol = piecePos.columnInt;
+
+        BoardPlace oneSpaceForward = piecePos.getTranslatedSpace(0, 1);
+        BoardPlace twoSpaceForward = piecePos.getTranslatedSpace(0, 2);
+        BoardPlace diagonalRight = piecePos.getTranslatedSpace(1, 1);
+        BoardPlace diagonalLeft = piecePos.getTranslatedSpace(-1, 1);
+
+        if ( tempRow == 2 && isWhite || tempRow == 7 && !isWhite) {
+            if ( oneSpaceForward.isEmpty() )
+                movesList.add( oneSpaceForward );
+            if ( twoSpaceForward.isEmpty() )
+                movesList.add( twoSpaceForward );
+        } else if ( oneSpaceForward.isEmpty() ) {
+            movesList.add( oneSpaceForward );
+        }
+        if (
+           !diagonalRight.isEmpty()
+           && diagonalRight.hasEnemy( isWhite )
+        ) {
+            movesList.add(diagonalRight);
+        }
+        if (
+            !diagonalLeft.isEmpty()
+            && diagonalLeft.hasEnemy( isWhite )
+        ) {
+            movesList.add(diagonalLeft);
+        }
+        return movesList.toArray( new BoardPlace[0]); //movesList.size()
     }
 }
