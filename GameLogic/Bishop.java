f942df58ec7f49dcad759502b98f823fb504e34b
GabrielQSherman <gabrielquinn.photography@gmail.com>
2020-11-04 19:15:06 -0500

    complete algorithm to calculate possible moves for Rook and Bishop

--- GameLogic/Bishop.java
+++ GameLogic/Bishop.java
@@ -1,5 +1,8 @@
 package com.company;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Bishop extends Piece {
 
     private String name;
@@ -21,7 +24,35 @@ public class Bishop extends Piece {
 
     @Override
     public BoardPlace[] getPossibleMoves( Board board) {
-        return new BoardPlace[0];
+        Piece movingPiece = this;
+
+        List<BoardPlace> movesList = new ArrayList<>();
+        boolean isWhite = movingPiece.isWhite;
+        BoardPlace piecePos = movingPiece.position;
+
+        int tempRow = piecePos.row;
+        int tempCol = piecePos.columnInt;
+
+        for (int i = -1; i <= 1; i++) {
+            for (int j = -1; j <= 1; j++) {
+                if ( !(i == 0 || j == 0) ){
+                    boolean hitBlock = false;
+                    int blocksMoved = 1;
+                    while  (!hitBlock) {
+                        BoardPlace possibleMove = board.getSpace(tempRow + blocksMoved * i, tempCol + blocksMoved * j );
+                        if (possibleMove != null && (possibleMove.isEmpty() )) {
+                            movesList.add(possibleMove);
+                            blocksMoved++;
+                        } else if (possibleMove != null && possibleMove.hasEnemy(isWhite)) {
+                            movesList.add(possibleMove);
+                            hitBlock = true;
+                        } else hitBlock = true;
+                    }
+                }
+            }
+        }
+
+        return movesList.toArray( new BoardPlace[0] );
     }
 
     @Override
--- GameLogic/Rook.java
+++ GameLogic/Rook.java
@@ -1,5 +1,8 @@
 package com.company;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Rook extends Piece {
     private String name;
     public Rook (BoardPlace startPos) {
@@ -13,7 +16,35 @@ public class Rook extends Piece {
 
     @Override
     public BoardPlace[] getPossibleMoves( Board board) {
-        return new BoardPlace[0];
+        Piece movingPiece = this;
+
+        List<BoardPlace> movesList = new ArrayList<>();
+        boolean isWhite = movingPiece.isWhite;
+        BoardPlace piecePos = movingPiece.position;
+
+        int tempRow = piecePos.row;
+        int tempCol = piecePos.columnInt;
+
+        for (int i = -1; i <= 1; i++) {
+            for (int j = -1; j <= 1; j++) {
+                if ( (i == 0 || j == 0) && !( i == 0 && j == 0) ) {
+                    boolean hitBlock = false;
+                    int blocksMoved = 1;
+                    while  (!hitBlock) {
+                        BoardPlace possibleMove = board.getSpace(tempRow + blocksMoved * i, tempCol + blocksMoved * j );
+                        if (possibleMove != null && (possibleMove.isEmpty() )) {
+                            movesList.add(possibleMove);
+                            blocksMoved++;
+                        } else if (possibleMove != null && possibleMove.hasEnemy(isWhite)) {
+                            movesList.add(possibleMove);
+                            hitBlock = true;
+                        } else hitBlock = true;
+                    }
+                }
+            }
+        }
+
+        return movesList.toArray( new BoardPlace[0] );
     }
 
     @Override
